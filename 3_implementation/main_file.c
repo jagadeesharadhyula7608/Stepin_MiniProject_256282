5d372d6c48e9c18016f9722bd950a3c731f4dee3
jagadeesharadhyula7608 <jagadeesharadhyula7608@gmail.com>
2021-04-13 19:24:40 +0530

    changes in code implementation

--- 3_implementation/main_file.c
+++ 3_implementation/main_file.c
@@ -1,16 +1,17 @@
 #include"stdio.h"
 #include"math.h"
 #include "line_parameters.h"
+#include "stdlib.h"
 int main()
 {
     int choice,ch; 
     while (1)
     {
-     int rcv_pow,rcv_vol,res,inductor;
-     double pf;
+     int rcv_pow,rcv_vol,len;
+     double pf,*ptr,res,inductor,cap;
      system("cls");
      printf("1. Compute Short Transmission Line Parameters\n");
-     printf("2. Compute Medium Transmission Line parameters\n"); 
+     printf("2. Compute Medium Transmission(Pi model) Line parameters\n"); 
      printf("3. Compute LongTransmission Line parameters\n");
      printf("4. Exit\n");
      printf("Enter your choice :");
@@ -25,23 +26,69 @@ int main()
                 printf("\nEnter the Receiving Line Voltage: ");
                 scanf("%d",&rcv_vol);
                 printf("\nEnter the Resistance of Transmission Line: ");
-                scanf("%d",&res);
+                scanf("%lf",&res);
                 printf("\nEnter the Inductance of the Transmission Line: ");
-                scanf("%d",&inductor);
+                scanf("%lf",&inductor);
                 printf("\nEnter  Power Factor: ");
                 scanf("%lf",&pf);
-                shortLine(rcv_pow,rcv_vol,pf,res,inductor);
+                ptr=shortLine(rcv_pow,rcv_vol,pf,res,inductor);
+                printf("...Sending end Voltage is %.3lf+i%.3lf\n",*ptr,*(ptr+1));
+                printf("...The efficieny of Short transmission Line is %.3lf\n",*(ptr+2));
+                printf("...The voltage regulation of short transmission line is %.3lf\n",*(ptr+3));
+                free(ptr);
                 printf("Press any key to continue\n");
                 ch=getch();
-                
                 break;
         case 2:
+                printf("........Computing Medium Transmission Line Parameters(Nominal Pi model)........\n");
+                printf("\nEnter the Resistance of Transmission Line per km: ");
+                scanf("%lf",&res);
+                printf("\nEnter the Inductance of the Transmission Line per km: ");
+                scanf("%lf",&inductor);
+                printf("\nEnter the Capacitive Suspectance of the Transmission Line per km: ");
+                scanf("%lf",&cap);
+                printf("\nEnter the Receiving Power: ");
+                scanf("%d",&rcv_pow);
+                printf("\nEnter the Receiving Line Voltage: ");
+                scanf("%d",&rcv_vol);
+                printf("\nEnter  Power Factor: ");
+                scanf("%lf",&pf);
+                printf("\nEnter the length of the Transmission Line: ");
+                scanf("%d",&len);
+                ptr=mediumLine(rcv_pow,rcv_vol,pf,res,inductor,cap,len);
+                printf("...Sending end Voltage is %.3lf+i%.3lf\n",*ptr,*(ptr+1));
+                printf("...Sending end Current is %.3lf+i%.3lf\n",*(ptr+2),*(ptr+3));
+                printf("...Sending end Power Factor is %.3lf\n",*(ptr+4));
+                printf("...The efficieny of Medium transmission Line is %.3lf\n",*(ptr+5));
+                printf("...The voltage regulation of Medium transmission line is %.3lf\n",*(ptr+6));
+                free(ptr);   
                 printf("Press any key to continue\n");
-                ch=getchar();
+                ch=getch();
                 break;
         case 3:
+                printf("..............Computing Long Transmission Line Parameters...........\n");
+                 printf("\nEnter the Resistance of Transmission Line: ");
+                scanf("%lf",&res);
+                printf("\nEnter the Inductance of the Transmission Line: ");
+                scanf("%lf",&inductor);
+                printf("\nEnter the Capacitive Suspectance of the Transmission Line per km: ");
+                scanf("%lf",&cap);
+                printf("\nEnter the Receiving Power: ");
+                scanf("%d",&rcv_pow);
+                printf("\nEnter the Receiving Line Voltage: ");
+                scanf("%d",&rcv_vol);
+                printf("\nEnter  Power Factor: ");
+                scanf("%lf",&pf);
+                printf("\nEnter the length of the Transmission Line: ");
+                scanf("%d",&len);
+              ptr=longLine(rcv_pow,rcv_vol,pf,res,inductor,cap,len);
+                printf("...Sending end Voltage is %lf+i%lf\n",*ptr,*(ptr+1));
+                printf("...Sending end Current is %lf+i%lf\n",*(ptr+2),*(ptr+3));
+                printf("...The efficieny of Long transmission Line is %lf\n",*(ptr+4));
+                printf("...The voltage regulation if Long transmission line is %lf\n",*(ptr+5));
+                free(ptr);
                 printf("Press any key to continue\n");
-                ch=getchar();
+                ch=getch();
                 break;
         case 4:
                 exit(0);
--- 3_implementation/src/line_parameters.c
+++ 3_implementation/src/line_parameters.c
@@ -1,6 +1,7 @@
 #include "stdio.h"
 #include "math.h"
 #include "line_parameters.h"
+#include "stdlib.h"
 
 c c_mul(c z,c i)
 {
@@ -14,10 +15,45 @@ double c_mag(c res)
 {
  return   (double) sqrt(pow(res.real,2)+pow(res.img,2));
 }
-
-void shortLine(int rcv_pow,int rcv_vol ,double pf,int res, int inductor )
+c c_add(c a ,c b)
 {
-    double sinteta,curr,eff,reg,send_vol,send_pow;
+    c res;
+    res.real=a.real+b.real;
+    res.img=a.img+b.img;
+    return res;
+}
+double c_angle(c a)
+{
+    double angle;
+    angle=atan(a.img/a.real);
+    return angle;
+
+}
+c c_div(c a,c b)
+{
+    c res;
+    if (b.img==0 && b.real==0)
+    {
+        res.real=0.0;
+        res.img=0.0;
+        return res;
+    }
+    else{
+        b.img=b.img*-1;
+        res.real=(1/c_mag(b))*(c_mul(a,b).real);
+        res.img=(1/c_mag(b))*(c_mul(a,b).img);
+        return res;
+    }
+c c_sqrt(c a)
+{
+
+}
+    
+}
+
+double * shortLine(int rcv_pow,int rcv_vol ,double pf,double res, double inductor )
+{
+    double sinteta,curr,eff,reg,send_vol,send_pow,*ptr1;
     c z,irbar,vs,r;
     rcv_vol=rcv_vol/sqrt(3);
     curr=rcv_pow/(3*rcv_vol*pf);
@@ -33,8 +69,100 @@ void shortLine(int rcv_pow,int rcv_vol ,double pf,int res, int inductor )
     send_pow=rcv_pow+((3*pow(curr,2)*res));
     eff=(rcv_pow/send_pow)*100;
     reg=((send_vol-rcv_vol)*100)/rcv_vol;
-    printf("\n");
-    printf("...Sending end Voltage is %lf+i%lf\n",vs.real,vs.img);
+    ptr1=(double *) calloc(4,sizeof(double));
+    *(ptr1)=vs.real;
+    *(ptr1+1)=vs.img;
+    *(ptr1+2)=eff;
+    *(ptr1+3)=reg;                                       
+ /*  printf("...Sending end Voltage is %lf+i%lf\n",vs.real,vs.img);
     printf("...The efficieny of Short transmission Line is %lf\n",eff);
-    printf("...The voltage regulation if short transmission line is %lf\n",reg);
+    printf("...The voltage regulation if short transmission line is %lf\n",reg); */
+    return ptr1;
+    
+}
+double * mediumLine(int rcv_pow,int rcv_vol ,double pf,double res, double inductor,double cap,int len )
+{
+    double sinteta,rcv_curr,eff,reg,send_vol,send_pow,*ptr,ang1,ang2,send_curr,pfs;
+    c z,Irbar,vs,r,Ic1bar,Ilinebar,Ic2bar,Isbar;
+    rcv_vol=rcv_vol/sqrt(3);
+    rcv_curr=rcv_pow/(3*rcv_vol*pf);
+    sinteta=sin(acos(pf));
+    z.real=res*len;
+    z.img=inductor*len;
+    Irbar.real=rcv_curr*pf;
+    Irbar.img=-rcv_curr*sinteta;
+    Ic1bar.real=0;
+    Ic1bar.img=0.5*len*cap*rcv_vol;
+    Ilinebar=c_add(Irbar,Ic1bar);
+    r=c_mul(z,Ilinebar);
+    vs.real=rcv_vol+r.real;
+    vs.img=r.img;
+    send_vol=c_mag(vs);
+    ang1=c_angle(vs);
+    Ic2bar.real=0.5*vs.img*cap*len;
+    Ic2bar.img=0.5*vs.real*cap*len;
+    Isbar=c_add(Ic2bar,Ilinebar);
+    send_curr=c_mag(Isbar);
+    ang2=c_angle(Isbar);
+    pfs=cos(fabs(ang1)+fabs(ang2));
+    send_pow=rcv_pow+((3*pow(send_curr,2)*res*0.5)+(3*pow(rcv_curr,2)*res*0.5));
+    eff=(rcv_pow/send_pow)*100;
+    reg=((send_vol-rcv_vol)*100)/rcv_vol;
+    ptr=(double *) calloc(7,sizeof(double));
+    *(ptr)=vs.real;
+    *(ptr+1)=vs.img;
+    *(ptr+2)=Isbar.real;
+    *(ptr+3)=Isbar.img;
+    *(ptr+4)=pfs;
+    *(ptr+5)=eff;
+    *(ptr+6)=reg;                                       
+ /*  printf("...Sending end Voltage is %lf+i%lf\n",vs.real,vs.img);
+    printf("...The efficieny of Short transmission Line is %lf\n",eff);
+    printf("...The voltage regulation if short transmission line is %lf\n",reg); */
+    return ptr;
+}
+double * longLine(int rcv_pow,int rcv_vol ,double pf,double res, double inductor,double cap,int len )
+{
+    double sinteta,rcv_curr,eff,reg,send_vol,send_pow,*ptr,send_curr;
+    c z,irbar,vs,r,ybar,A,B,C,D,vrbar,Isbar;
+    rcv_vol=rcv_vol/sqrt(3);
+    rcv_curr=rcv_pow/(3*rcv_vol*pf);
+    sinteta=sin(acos(pf));
+    z.real=0;
+    z.img=inductor;
+    ybar.real=0;
+    ybar.img=cap*len;
+    vrbar.real=rcv_vol;
+    vrbar.img=0;
+    irbar.real=rcv_curr*pf;
+    irbar.img=-rcv_curr*sinteta;
+    A.real=cosh(sqrt((int)(c_mul(ybar,z).real*-1)));
+    B.real=sqrt(c_div(z,ybar).real)*sinh(sqrt(-1*c_mul(ybar,z).real));
+    C.real=sqrt(c_div(z,ybar).real)*sinh(sqrt(-1*c_mul(ybar,z).real));
+    D.real=A.real;
+    A.img=0;B.img=0;C.img=0;D.img=0;
+    vs=c_add(c_mul(A,vrbar),c_mul(B,irbar));
+    Isbar=c_add(c_mul(C,vrbar),c_mul(D,irbar));
+    send_vol=c_mag(vs);
+    send_curr=c_mag(Isbar);
+    send_pow=rcv_pow+((3*pow(send_curr,2)*res*0.5)+(3*pow(rcv_curr,2)*res*0.5));
+    eff=(rcv_pow/send_pow)*100;
+    reg=(((send_vol/A.real)-rcv_vol)*100)/rcv_vol;
+    ptr=(double *) calloc(4,sizeof(double));
+    printf("%lf+i%lf  ",A.real,A.img);
+    printf("%lf+i%lf\n",B.real,B.img);
+    printf("%lf+i%lf  ",C.real,C.img);
+    printf("%lf+i%lf\n",D.real,D.img);
+    ptr=(double *) calloc(6,sizeof(double));
+    *(ptr)=vs.real;
+    *(ptr+1)=vs.img;
+    *(ptr+2)=Isbar.real;
+    *(ptr+3)=Isbar.img;
+    *(ptr+4)=eff;
+    *(ptr+5)=reg;                                      
+ /*  printf("...Sending end Voltage is %lf+i%lf\n",vs.real,vs.img);
+    printf("...The efficieny of Short transmission Line is %lf\n",eff);
+    printf("...The voltage regulation if short transmission line is %lf\n",reg); */
+    //printf("%lf",cosh(sqrt(c_mul(ybar,z).real*-1)));
+    return ptr;
 }
